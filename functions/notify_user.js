0617b4e567ef6606c445cf9f076b4bd1099fcb1a
Jón Tómas Grétarsson <jon.gretarsson@gmail.com>
2016-12-08 23:09:56 -0800

    Fixing up the fanout

--- functions/notify_user.js
+++ functions/notify_user.js
@@ -4,23 +4,20 @@
  */
 
 var AWS = require('aws-sdk');
-var lambda = new AWS.Lambda();
 
-// TODO: Iterate over notifications, and submit async lambda's to notify per-user.
+// TODO: Reach out to the provided user through their preferred notification platform.
+// TODO: SNS (push notifications)
+// TODO: Twilio (push-to-call)
+// TODO: SES (email)
 module.exports.handler = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
+      context: context,
     }),
   };
 
-  lambda.invoke({
-    FunctionName: process.env.SUBMIT_USER_FUNCTION_NAME,
-    InvocationType: 'Event',
-    Payload: JSON.stringify({
-      some_variable: 'some_value'
-    })
-  }, callback);
+  callback(null, response);
 };
