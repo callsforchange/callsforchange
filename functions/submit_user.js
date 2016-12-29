ff660ab2d35ae1bfb9ab452467e4f303a33818de
Jón Tómas Grétarsson <jon.gretarsson@gmail.com>
2016-12-29 11:20:37 -0800

    Fetch relevant information from the civic API

--- functions/submit_user.js
+++ functions/submit_user.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var Promise = require('bluebird');
+var civicinfo = require('../libs/google').civicinfo('v2');
 var mailchimp = require('../libs/mailchimp');
 
 module.exports.handler = (event, context, callback) => {
@@ -9,9 +11,20 @@ module.exports.handler = (event, context, callback) => {
     event: JSON.stringify(event),
   });
 
-  mailchimp.post(`/lists/${process.env.MAILCHIMP_LIST_ID}/members`, {
-    email_address: event.body.email,
-    status: 'subscribed'
+  Promise.promisify(civicinfo.representatives.representativeInfoByAddress)({
+    address: event.body.address,
+    levels: ['country'],
+    roles: ['legislatorLowerBody', 'legislatorUpperBody'],
+    fields: 'officials(channels,emails,name,phones,photoUrl)'
+  })
+
+  .then(data => {
+    console.log('Received some information from CIVIC API ' + JSON.stringify(data));
+
+    return mailchimp.post(`/lists/${process.env.MAILCHIMP_LIST_ID}/members`, {
+      email_address: event.body.email,
+      status: 'subscribed'
+    })
   })
 
   .then(data => {
