de465b92a9bcf84dc32efe667f4b2452abaa87f7
Jón Tómas Grétarsson <jon.gretarsson@gmail.com>
2016-12-30 23:14:43 -0800

    Moving list field registration into a method and beefing it up to handle patching, and stubbing out webhook subscriptions

--- functions/migrate.js
+++ functions/migrate.js
@@ -8,50 +8,73 @@
 var mailchimp = require('../libs/mailchimp');
 var AWS = require('aws-sdk');
 
-var expected_merge_fields = [
-  { name: 'REP1_NAME',  tag: 'REP1_NAME',  type: 'text' },
-  { name: 'REP1_PHOTO', tag: 'REP1_PHOTO', type: 'imageurl', required: true },
-  { name: 'REP1_PHONE', tag: 'REP1_PHONE', type: 'phone',    required: true, options: { phone_format: 'US' } },
-  { name: 'REP2_NAME',  tag: 'REP2_NAME',  type: 'text' },
-  { name: 'REP2_PHOTO', tag: 'REP2_PHOTO', type: 'imageurl', required: true },
-  { name: 'REP2_PHONE', tag: 'REP2_PHONE', type: 'phone',    required: true, options: { phone_format: 'US' } },
-  { name: 'REP3_NAME',  tag: 'REP3_NAME',  type: 'text' },
-  { name: 'REP3_PHOTO', tag: 'REP3_PHOTO', type: 'imageurl', required: true },
-  { name: 'REP3_PHONE', tag: 'REP3_PHONE', type: 'phone',    required: true, options: { phone_format: 'US' } }
-];
+const expected_merge_fields = {
+  REP1_NAME:  { name: 'REP1_NAME',  tag: 'REP1_NAME',  type: 'text' },
+  REP1_PHOTO: { name: 'REP1_PHOTO', tag: 'REP1_PHOTO', type: 'imageurl', required: true },
+  REP1_PHONE: { name: 'REP1_PHONE', tag: 'REP1_PHONE', type: 'phone',    required: true, options: { phone_format: 'US' } },
+  REP2_NAME:  { name: 'REP2_NAME',  tag: 'REP2_NAME',  type: 'text' },
+  REP2_PHOTO: { name: 'REP2_PHOTO', tag: 'REP2_PHOTO', type: 'imageurl', required: true },
+  REP2_PHONE: { name: 'REP2_PHONE', tag: 'REP2_PHONE', type: 'phone',    required: true, options: { phone_format: 'US' } },
+  REP3_NAME:  { name: 'REP3_NAME',  tag: 'REP3_NAME',  type: 'text' },
+  REP3_PHOTO: { name: 'REP3_PHOTO', tag: 'REP3_PHOTO', type: 'imageurl', required: true },
+  REP3_PHONE: { name: 'REP3_PHONE', tag: 'REP3_PHONE', type: 'phone',    required: true, options: { phone_format: 'US' } }
+};
+
+function list_field_migrate() {
+  // default is 10, but with FNAME and LNAME already available by default, lets just go to 50 and call it a day
+  return mailchimp.get(`/lists/${process.env.MAILCHIMP_LIST_ID}/merge-fields?count=50`)
+  .then(fields => {
+    const existing_field_tags = new Map(fields.merge_fields.map(f => [f.tag, f]));
+    console.log('Existing fields: ' + JSON.stringify(Array.from(existing_field_tags.keys())));
+
+    const missing_fields = Object.keys(expected_merge_fields)
+      .filter(tag => !existing_field_tags.has(tag))
+      .map(tag => expected_merge_fields[tag]);
+    console.log('Missing merge fields: ' + JSON.stringify(missing_fields));
+
+    const missing_fields_promises = missing_fields
+      .map(missing_field => mailchimp.post(`/lists/${process.env.MAILCHIMP_LIST_ID}/merge-fields`, missing_field));
+
+    const modified_fields = Object.keys(expected_merge_fields)
+      .filter(tag => existing_field_tags.has(tag))
+      .map(tag => expected_merge_fields[tag])
+      .filter(f => existing_field_tags.get(f.tag).name !== f.name || existing_field_tags.get(f.tag).type !== f.type)
+    console.log('Modified merge fields: ' + JSON.stringify(modified_fields));
+
+    const modified_fields_promises = modified_fields
+      .map(modified_field => mailchimp.patch(`/lists/${process.env.MAILCHIMP_LIST_ID}/merge-fields/${existing_field_tags.get(f.tag).merge_id}`, modified_field));
+
+    return Promise.all(missing_fields_promises.concat(modified_fields_promises));
+  });
+}
+
+const expected_webhooks = {
+  subscribe: {},
+  unsubscribe: {}
+};
+
+function list_webhook_register() {
+  // return mailchimp.get(`/lists/${process.env.MAILCHIMP_LIST_ID}/
+}
 
 module.exports.handler = (event, context, callback) => {
-  // default is 10, but with FNAME and LNAME already available by default, lets just go to 50 and call it a day
-  mailchimp.get(`/lists/${process.env.MAILCHIMP_LIST_ID}/merge-fields?count=50`)
-  .then(fields => {
-    var existing_field_tags = new Set(fields.merge_fields.map(f => f.tag));
+  Promise.all([
+    list_field_migrate()
 
-    console.log(JSON.stringify(fields));
-    console.log('Detected tags: ' + JSON.stringify(Array.from(existing_field_tags)));
-    console.log('Missing merge fields: ' + JSON.stringify(expected_merge_fields .filter(f => !existing_field_tags.has(f.tag) ) ) );
-
-    return Promise.all(expected_merge_fields
-      .filter(f => !existing_field_tags.has(f.tag) )
-      .map(missing_field => mailchimp.post(`/lists/${process.env.MAILCHIMP_LIST_ID}/merge-fields`, missing_field)));
-
-  }).then(values => {
-    console.log(`Fields properly created! ${JSON.stringify(values)}`);
-    callback(null, {
-      event: event,
-      context: context,
-      values: values 
-    });
+  ]).then(values => {
+    callback(null, { values: values });
   })
 
   .catch(error => {
     if (error.error) {
       console.log(error.code + ": " + error.error);
     } else {
-      console.log('There was an error creating custom mailchimp fields!');
+      console.log('There was an error performing migrations!');
     }
     callback(JSON.stringify({
       message: 'There was an error!',
       error: error
     }));
   });
+
 };
