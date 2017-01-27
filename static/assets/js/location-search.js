07a3857dad697479fbea8c7398aa4044aba47fe0
Greg Allen <greg@firstandthird.com>
2017-01-26 21:59:27 -0800

    initial work on auto complete - still needs styles. #65

--- static/assets/js/location-search.js
+++ static/assets/js/location-search.js
@@ -1,1 +1,28 @@
-console.log('hi');
+import Complete from '@firstandthird/complete';
+
+class LocationSearch extends Complete {
+  preInit() {
+    window.initAutocomplete = () => {
+      this.loaded = true;
+      this.service = new window.google.maps.places.AutocompleteService();
+    }
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${this.options.apikey}&libraries=places&&callback=initAutocomplete`
+    document.head.appendChild(script);
+  }
+  fetch() {
+    console.log('searching');
+    if (!this.service) {
+      return;
+    }
+    this.service.getQueryPredictions({ input: this.term }, (results) => {
+      const addresses = results.map((result) => ({ 
+        name: result.description, 
+        value: result.description 
+      }));
+      this.render(addresses);
+    });
+  }
+}
+
+Complete.register('LocationSearch', LocationSearch);
